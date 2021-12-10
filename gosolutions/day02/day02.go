973b8453c5ea40e9636406f36b03642f16c2aa86
Riley John Gibbs <rileyjohngibbs@gmail.com>
2021-12-10 14:30:09 -0800

    Finish day 2 Go solution

--- gosolutions/day02/day02.go
+++ gosolutions/day02/day02.go
@@ -19,10 +19,15 @@ func main() {
         panic(err)
     }
     defer file.Close()
+
     scanner := bufio.NewScanner(file)
 
     horizontal := 0
     depth := 0
+
+    aim2 := 0
+    horizontal2 := 0
+    depth2 := 0
 
     for scanner.Scan() {
         tokens := strings.Fields(scanner.Text())
@@ -30,12 +35,17 @@ func main() {
         value, _ := strconv.Atoi(tokens[1])
         if direction == "forward" {
             horizontal += value
+            horizontal2 += value
+            depth2 += aim2 * value
         } else if direction == "up" {
             depth -= value
+            aim2 -= value
         } else if direction == "down" {
             depth += value
+            aim2 += value
         }
     }
 
     fmt.Println(horizontal * depth)
+    fmt.Println(horizontal2 * depth2)
 }
