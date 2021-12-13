879afd8cecbbcb2c43ea1b38dacc0a9cc85b688a
Riley John Gibbs <rileyjohngibbs@gmail.com>
2021-12-13 00:16:18 -0800

    Day 13 solution in Python

---
+++ solutions/day13/alpha.py
@@ -0,0 +1,116 @@
+from dataclasses import dataclass
+from enum import Enum
+import pytest
+
+
+def digest_input(input_lines: list[str]) -> tuple[list["Point"], list["Fold"]]:
+    points, folds = [], []
+    section = "points"
+    for input_line in input_lines:
+        if not input_line:
+            section = "folds"
+        elif section == "points":
+            str_x, str_y = input_line.split(",", 1)
+            points.append(Point(int(str_x), int(str_y)))
+        elif section == "folds":
+            var, value = input_line[11:].split("=", 1)
+            direction = (
+                FoldDirection.VERTICAL if var == "y" else FoldDirection.HORIZONTAL
+            )
+            folds.append(Fold(direction, int(value)))
+    return points, folds
+
+
+@dataclass(frozen=True)
+class Point:
+    x: int
+    y: int
+
+
+class FoldDirection(Enum):
+    VERTICAL = 0
+    HORIZONTAL = 1
+
+
+@dataclass
+class Fold:
+    direction: FoldDirection
+    coordinate: int
+
+    def should_reflect(self, point: "Point") -> bool:
+        return (
+            self.direction is FoldDirection.VERTICAL
+            and point.y > self.coordinate
+            or self.direction is FoldDirection.HORIZONTAL
+            and point.x > self.coordinate
+        )
+
+
+def part_one(points_folds: tuple[list["Point"], list["Fold"]]) -> int:
+    points = set(points_folds[0])
+    folds = points_folds[1]
+    for fold in folds:
+        points_to_reflect = [p for p in points if fold.should_reflect(p)]
+        for point in points_to_reflect:
+            points.remove(point)
+            points.add(reflect(point, fold))
+        break
+    return len(points)
+
+
+def part_two(points_folds: tuple[list["Point"], list["Fold"]]) -> None:
+    points = set(points_folds[0])
+    folds = points_folds[1]
+    for fold in folds:
+        points_to_reflect = [p for p in points if fold.should_reflect(p)]
+        for point in points_to_reflect:
+            points.remove(point)
+            points.add(reflect(point, fold))
+    width, height = (max(p.x for p in points) + 1, max(p.y for p in points) + 1)
+    for y in range(height):
+        for x in range(width):
+            if Point(x, y) in points:
+                print("#", end="")
+            else:
+                print(".", end="")
+        print()
+    return None
+
+
+def reflect(point: "Point", fold: "Fold") -> "Point":
+    return (
+        Point(2 * fold.coordinate - point.x, point.y)
+        if fold.direction is FoldDirection.HORIZONTAL
+        else Point(point.x, 2 * fold.coordinate - point.y)
+    )
+
+
+@pytest.fixture
+def example():
+    return [
+        "6,10",
+        "0,14",
+        "9,10",
+        "0,3",
+        "10,4",
+        "4,11",
+        "6,0",
+        "6,12",
+        "4,1",
+        "0,13",
+        "10,12",
+        "3,4",
+        "3,0",
+        "8,4",
+        "1,10",
+        "2,14",
+        "8,10",
+        "9,0",
+        "",
+        "fold along y=7",
+        "fold along x=5",
+    ]
+
+
+def test_part_one(example):
+    assert part_one(digest_input(example)) == 17
